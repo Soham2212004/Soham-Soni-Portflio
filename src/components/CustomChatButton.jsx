00bced59d0a0c12bd29f1bb322802288670d6d1f
Soham Soni <142389695+Soham2212004@users.noreply.github.com>
2025-11-14 10:10:02 +0530

    Add responsive styles to CustomChatButton

--- src/components/CustomChatButton.jsx
+++ src/components/CustomChatButton.jsx
@@ -33,7 +33,9 @@ const CustomChatButton = ({ onClick, isOpen }) => {
         onClick={onClick}
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
-        className={`fixed bottom-6 right-6 w-16 h-16 rounded-full shadow-2xl z-50 flex items-center justify-center transition-all duration-300 ease-in-out ${buttonBg}`}
+        className={`fixed bottom-6 right-6 w-16 h-16 rounded-full shadow-2xl z-50 flex items-center justify-center transition-all duration-300 ease-in-out ${buttonBg}
+        sm:bottom-5 sm:right-5 sm:w-14 sm:h-14
+        xs:bottom-4 xs:right-4 xs:w-12 xs:h-12`}
         style={
           mode === 'sunset'
             ? {
@@ -61,6 +63,7 @@ const CustomChatButton = ({ onClick, isOpen }) => {
               viewBox="0 0 24 24"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
+              className="sm:w-5 sm:h-5 xs:w-4 xs:h-4"
             >
               <path
                 d="M18 6L6 18M6 6L18 18"
@@ -78,6 +81,7 @@ const CustomChatButton = ({ onClick, isOpen }) => {
               viewBox="0 0 24 24"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
+              className="sm:w-6 sm:h-6 xs:w-5 xs:h-5"
             >
               <path
                 d="M21 11.5C21.0034 12.8199 20.6951 14.1219 20.1 15.3C19.3944 16.7118 18.3098 17.8992 16.9674 18.7293C15.6251 19.5594 14.0782 19.9994 12.5 20C11.1801 20.0035 9.87812 19.6951 8.7 19.1L3 21L4.9 15.3C4.30493 14.1219 3.99656 12.8199 4 11.5C4.00061 9.92179 4.44061 8.37488 5.27072 7.03258C6.10083 5.69028 7.28825 4.60571 8.7 3.90003C9.87812 3.30496 11.1801 2.99659 12.5 3.00003H13C15.0843 3.11502 17.053 3.99479 18.5291 5.47089C20.0052 6.94699 20.885 8.91568 21 11V11.5Z"
@@ -92,9 +96,10 @@ const CustomChatButton = ({ onClick, isOpen }) => {
         </motion.div>
       </motion.button>
 
-      {/* Tooltip */}
+      {/* Tooltip - Hidden on mobile */}
       <motion.div
-        className={`fixed bottom-6 right-24 px-3 py-2 rounded-lg shadow-lg z-40 whitespace-nowrap ${tooltipBg} ${tooltipText}`}
+        className={`fixed bottom-6 right-24 px-3 py-2 rounded-lg shadow-lg z-40 whitespace-nowrap ${tooltipBg} ${tooltipText}
+        sm:hidden`}
         initial={{ opacity: 0, x: 20 }}
         animate={{
           opacity: isHovered && !isOpen ? 1 : 0,
